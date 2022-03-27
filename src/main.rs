a28995824ece5db448c98a07c7faef6eb5a384d1
Rotem Yaari <vmalloc@gmail.com>
2022-03-27 15:59:21 +0300

    Make more resilient to disconnections

--- src/main.rs
+++ src/main.rs
@@ -69,7 +69,9 @@ enum Command {
 }
 
 async fn entry_point(opts: Opts) -> Result<()> {
-    match opts.command {
+    let retry_policy = again::RetryPolicy::exponential(Duration::from_secs(1));
+
+    match &opts.command {
         Command::Consume {
             subscription_name,
             consumer_name,
@@ -82,33 +84,43 @@ async fn entry_point(opts: Opts) -> Result<()> {
             shared,
             ack,
         } => {
-            let url = opts.url;
-            let builder = Pulsar::builder(url.as_str(), TokioExecutor)
-                .build()
-                .await?
-                .consumer()
-                .with_consumer_name(consumer_name)
-                .with_subscription(subscription_name)
-                .with_subscription_type(if shared {
-                    SubType::Shared
-                } else {
-                    SubType::Exclusive
-                })
-                .with_topic(topic)
-                .with_options(ConsumerOptions {
-                    durable: Some(durable),
-                    initial_position: if earliest {
-                        InitialPosition::Earliest
-                    } else {
-                        InitialPosition::default()
-                    },
-                    ..Default::default()
-                });
+            let mut consumer = retry_policy
+                .retry(|| async {
+                    let builder = Pulsar::builder(opts.url.as_str(), TokioExecutor)
+                        .build()
+                        .await
+                        .map_err(|e| {
+                            log::error!("Failed connecting to Pulsar: {:?}", e);
+                            e
+                        })?
+                        .consumer()
+                        .with_consumer_name(consumer_name)
+                        .with_subscription(subscription_name)
+                        .with_subscription_type(if *shared {
+                            SubType::Shared
+                        } else {
+                            SubType::Exclusive
+                        })
+                        .with_topic(topic)
+                        .with_options(ConsumerOptions {
+                            durable: Some(*durable),
+                            initial_position: if *earliest {
+                                InitialPosition::Earliest
+                            } else {
+                                InitialPosition::default()
+                            },
+                            ..Default::default()
+                        });
 
-            let mut consumer = builder.build::<Vec<u8>>().await?;
+                    builder.build::<Vec<u8>>().await.map_err(|e| {
+                        log::error!("Error trying to connect: {:?}. Retrying...", e);
+                        e
+                    })
+                })
+                .await?;
 
             let mut forward_producer = if let Some(topic) = forward_to_topic {
-                let url = forward_to_url.as_ref().unwrap_or(&url);
+                let url = forward_to_url.as_ref().unwrap_or(&opts.url);
                 Some(
                     Pulsar::builder(url.as_str(), TokioExecutor)
                         .build()
@@ -122,67 +134,68 @@ async fn entry_point(opts: Opts) -> Result<()> {
                 None
             };
 
-            while let Some(message) = consumer.try_next().await? {
-                let publish_time = message
-                    .metadata()
-                    .event_time
-                    .unwrap_or_else(|| message.metadata().publish_time);
-                let publish_time = DateTime::<Utc>::from_utc(
-                    NaiveDateTime::from_timestamp(
-                        (publish_time / 1000) as i64,
-                        ((publish_time % 1000) * 1_000_000) as u32,
-                    ),
-                    Utc,
-                );
-                println!("-- {}:", publish_time);
-                if !message.metadata().properties.is_empty() {
-                    for item in message.metadata().properties.iter() {
-                        println!(
-                            "{}{}={}{}",
-                            color::Fg(color::Magenta),
-                            item.key,
-                            item.value,
-                            color::Fg(color::Reset)
-                        );
-                    }
-                }
-                if json {
-                    match serde_json::from_slice::<Value>(&message.payload.data) {
-                        Ok(val) => println!("{}", to_colored_json_auto(&val).unwrap()),
-                        Err(_) => eprintln!(
-                            "{}Value {:?} is not JSON{}",
-                            color::Fg(color::Red),
-                            String::from_utf8_lossy(&message.payload.data),
-                            color::Fg(color::Reset)
+            loop {
+                if let Some(message) = consumer.try_next().await? {
+                    let publish_time = message
+                        .metadata()
+                        .event_time
+                        .unwrap_or_else(|| message.metadata().publish_time);
+                    let publish_time = DateTime::<Utc>::from_utc(
+                        NaiveDateTime::from_timestamp(
+                            (publish_time / 1000) as i64,
+                            ((publish_time % 1000) * 1_000_000) as u32,
                         ),
+                        Utc,
+                    );
+                    println!("-- {}:", publish_time);
+                    if !message.metadata().properties.is_empty() {
+                        for item in message.metadata().properties.iter() {
+                            println!(
+                                "{}{}={}{}",
+                                color::Fg(color::Magenta),
+                                item.key,
+                                item.value,
+                                color::Fg(color::Reset)
+                            );
+                        }
                     }
-                } else {
-                    println!("{}", String::from_utf8_lossy(&message.payload.data));
-                }
+                    if *json {
+                        match serde_json::from_slice::<Value>(&message.payload.data) {
+                            Ok(val) => println!("{}", to_colored_json_auto(&val).unwrap()),
+                            Err(_) => eprintln!(
+                                "{}Value {:?} is not JSON{}",
+                                color::Fg(color::Red),
+                                String::from_utf8_lossy(&message.payload.data),
+                                color::Fg(color::Reset)
+                            ),
+                        }
+                    } else {
+                        println!("{}", String::from_utf8_lossy(&message.payload.data));
+                    }
 
-                if let Some(forwarder) = forward_producer.as_mut() {
-                    forwarder
-                        .send(pulsar::producer::Message {
-                            payload: message.payload.data.clone(),
-                            properties: message
-                                .payload
-                                .metadata
-                                .properties
-                                .iter()
-                                .cloned()
-                                .map(|i| (i.key, i.value))
-                                .collect(),
-                            event_time: Some(publish_time.timestamp_millis() as u64),
-                            ..Default::default()
-                        })
-                        .await?;
-                }
+                    if let Some(forwarder) = forward_producer.as_mut() {
+                        forwarder
+                            .send(pulsar::producer::Message {
+                                payload: message.payload.data.clone(),
+                                properties: message
+                                    .payload
+                                    .metadata
+                                    .properties
+                                    .iter()
+                                    .cloned()
+                                    .map(|i| (i.key, i.value))
+                                    .collect(),
+                                event_time: Some(publish_time.timestamp_millis() as u64),
+                                ..Default::default()
+                            })
+                            .await?;
+                    }
 
-                if ack {
-                    consumer.ack(&message).await?;
+                    if *ack {
+                        consumer.ack(&message).await?;
+                    }
                 }
             }
-            Ok(())
         }
 
         Command::Produce {
@@ -203,17 +216,21 @@ async fn entry_point(opts: Opts) -> Result<()> {
                 })
                 .collect::<Result<HashMap<_, _>>>()?;
 
-            let mut producer = Pulsar::builder(opts.url.as_str(), TokioExecutor)
-                .build()
-                .await?
-                .producer()
-                .with_topic(topic)
-                .with_name(producer_name)
-                .build()
+            let mut producer = retry_policy
+                .retry(|| async {
+                    Pulsar::builder(opts.url.as_str(), TokioExecutor)
+                        .build()
+                        .await?
+                        .producer()
+                        .with_topic(topic)
+                        .with_name(producer_name)
+                        .build()
+                        .await
+                })
                 .await?;
             info!("Connected to Pulsar");
             for i in 0.. {
-                tokio::time::sleep(interval.into()).await;
+                tokio::time::sleep((*interval).into()).await;
                 let payload = serde_json::to_vec(&json!({
                     "iteration": i,
                     "timestamp": Utc::now(),
